334b6b562e618757a0a017c89549ca9c408a0aba
no-cy <nochanyoung0@gmail.com>
2024-02-12 20:26:03 +0900

    프로젝트 생성

---
+++ src/main/java/spring/marketnori/MarketNoriApplication.java
@@ -0,0 +1,13 @@
+package spring.marketnori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketNoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketNoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/marketnori/MarketNoriApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.marketnori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketNoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
