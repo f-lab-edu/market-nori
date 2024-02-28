e8caf29ef72a60b6614e02a1926e9e0a5d6abd3c
no-cy <63999547+no-cy@users.noreply.github.com>
2024-02-28 23:26:17 +0900

    [#12] Spring Security 설정 (#13)
    * [#12] Spring Security dependency 추가
    
    - spring-security-web
    - spring-security-config
    
    * [#12] Spring Security dependency 추가
    
    - spring-boot-starter-security
    
    * [#12] Spring Security SecurityConfig 클래스 추가 (임시)
    
    - 스프링 시큐리티 인증,인가 방식을 사용하기 위해 SecurityConfig 추가
    - 추후 핵심 기능들이 개발되면 JWT 토큰 인증 방식 구현할 예정
    - 현재는 사용하지 않을 기능이기에 @Configureation, @EnableWebSecurity 주석 처리

---
+++ src/main/java/spring/marketnori/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package spring.marketnori.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+//@EnableWebSecurity // 기본적인 웹 보안 활성화
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //csrf disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        //From 로그인 방식 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/v1/login", "/v1/users/join").permitAll()
+                        .anyRequest().authenticated());
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
