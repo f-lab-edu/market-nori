bdaf7861b6a502c0fa28f0de908e20a40f109bf9
no-cy <nochanyoung0@gmail.com>
2024-03-04 00:01:29 +0900

    [#16] 상품 리스트 조회 JPA 메서드 추가
    * 상품 리스트 조회 JPA 메서드 추가
    * 상품 리스트 조회 JPQL 작성

--- src/main/java/spring/marketnori/productcategory/ProductCategoryRepository.java
+++ src/main/java/spring/marketnori/productcategory/ProductCategoryRepository.java
@@ -1,6 +1,11 @@
 package spring.marketnori.productcategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    @Query("SELECT c from ProductCategory c join fetch c.products")
+    List<ProductCategory> findAllCategoriesWithProducts();
 }
