b9ceb3c3644ce5a91583d4688ce7ad95729268fa
no-cy <nochanyoung0@gmail.com>
2024-03-12 00:26:32 +0900

    [#24] 상품 리스트 조회 시 순환참조 발생 현상 이슈 개선
    * 양방향 관계를 맺은 두 엔티티 객체가 서로를 직접 참조하지 않도록 DTO 객체 생성 (ProductCategoryDto, ProductDto)
    * 서비스 계층에서 엔티티 객체의 데이터를 Dto 객체에게 전달하기 위한 'convertToDto()' 메서드 추가

--- src/main/java/spring/marketnori/productcategory/ProductCategoryController.java
+++ src/main/java/spring/marketnori/productcategory/ProductCategoryController.java
@@ -2,10 +2,14 @@ package spring.marketnori.productcategory;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import spring.marketnori.productcategory.dto.ProductListResponse;
+
+import java.util.List;
 
 
 @Slf4j
@@ -21,11 +25,21 @@ public class ProductCategoryController {
 
     @ResponseBody
     @GetMapping("/v1/products")
-    public ProductListResponse listProducts() {
-        ProductListResponse productListResponse = new ProductListResponse();
-        productListResponse.setCategories(productCategoryService.findProductsByCategory());
+    public ResponseEntity<ProductListResponse> listProducts() {
+        try {
+            ProductListResponse productListResponse = new ProductListResponse();
+            List<ProductListResponse.ProductCategoryDto> categories = productCategoryService.findProductsByCategory();
 
-        return productListResponse;
+            if (categories.isEmpty()) {
+                log.debug("The product list is empty.");
+                return ResponseEntity.noContent().build();
+            }
+
+            productListResponse.setCategories(categories);
+            return ResponseEntity.ok(productListResponse);
+        } catch (Exception e) {
+            log.error("Error retrieving product list", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
-
 }
