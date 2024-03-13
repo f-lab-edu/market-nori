53bb4d6050edddcb83ff3d23b2a89f1d5bf09fe1
no-cy <63999547+no-cy@users.noreply.github.com>
2024-03-13 23:59:22 +0900

    [#26] 상품 검색 리포지토리 생성 (#27)
    * Spring Data JPA 사용
    * 'LIKE' 절을 사용하여 키워드가 포함된 상품들을 조회할 수 있도록 JPQL 작성

--- src/main/java/spring/marketnori/product/ProductRepository.java
+++ src/main/java/spring/marketnori/product/ProductRepository.java
@@ -1,6 +1,12 @@
 package spring.marketnori.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p from Product p WHERE p.name LIKE CONCAT('%', :keyword, '%')")
+    List<Product> findProductBy(@Param("keyword") String keyword);
 }
